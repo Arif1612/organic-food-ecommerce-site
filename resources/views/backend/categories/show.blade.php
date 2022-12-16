bdcbd9bd067aac50761e289b4f20bcc63eb2b2e7
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2022-12-16 23:28:23 +0600

    ki j kori

--- resources/views/backend/categories/show.blade.php
+++ resources/views/backend/categories/show.blade.php
@@ -3,6 +3,7 @@
     <x-slot:title>
         Categories Show
     </x-slot:title>
+    <p>ki j kori maje maje</p>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Categories</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
