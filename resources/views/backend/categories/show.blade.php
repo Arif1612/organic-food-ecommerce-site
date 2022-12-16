d5498c697c2f08f56e5d0b31b8aa4f1bcc00e344
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2022-12-16 23:18:40 +0600

    another check

--- resources/views/backend/categories/show.blade.php
+++ resources/views/backend/categories/show.blade.php
@@ -4,6 +4,7 @@
         Categories Show
     </x-slot:title>
     <h1>Checking</h1>
+    <h2>Another Check</h2>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Categories</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
