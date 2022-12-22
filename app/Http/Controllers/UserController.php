76dc41c12156b445ea941163b0258401c7e51f60
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2022-12-22 14:19:29 +0600

    authentication in all routes done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('backend.users.index');
+        $users = User::latest()->paginate(10);
+        return view('backend.users.index', compact('users'));
     }
 
     /**
@@ -43,9 +45,9 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return view('backend.users.show', compact('user'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,64 +18,71 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
 
-Route::get('/categories/pdf', [CategoryController::class, 'downloadPdf'])->name('categories.pdf');
-Route::get('/categories/excel', [CategoryController::class, 'downloadExcel'])->name('categories.excel');
 
-// Softdelte Route
-Route::get('/categories-trash', [CategoryController::class, 'trash'])->name('categories.trash');
-Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
-Route::delete('/categories/{id}/permanent/delete', [CategoryController::class, 'delete'])->name('categories.delete');
-Route::get('/categories/{id}/softDeleteShow', [CategoryController::class, 'softDeleteShow'])->name('categories.softDeleteShow');
+Route::middleware('auth')->group(function () {
 
-// Categories Crud route
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
-Route::delete('/categories/{id}/delete', [CategoryController::class, 'destroy'])->name('categories.destroy');
-Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::patch('/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-// products
-
-Route::get('/products/pdf', [ProductController::class, 'downloadPdf'])->name('products.pdf');
-Route::get('/products/excel', [ProductController::class, 'downloadExcel'])->name('products.excel');
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
 
-// Softdelte Route
-Route::get('/products-trash', [ProductController::class, 'trash'])->name('products.trash');
-Route::get('/products/{id}/restore', [ProductController::class, 'restore'])->name('products.restore');
-Route::delete('/products/{id}/permanent/delete', [ProductController::class, 'delete'])->name('products.delete');
-Route::get('/products/{id}/softDeleteShow', [ProductController::class, 'softDeleteShow'])->name('products.softDeleteShow');
+    Route::get('/categories/pdf', [CategoryController::class, 'downloadPdf'])->name('categories.pdf');
+    Route::get('/categories/excel', [CategoryController::class, 'downloadExcel'])->name('categories.excel');
+
+    // Softdelte Route
+    Route::get('/categories-trash', [CategoryController::class, 'trash'])->name('categories.trash');
+    Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+    Route::delete('/categories/{id}/permanent/delete', [CategoryController::class, 'delete'])->name('categories.delete');
+    Route::get('/categories/{id}/softDeleteShow', [CategoryController::class, 'softDeleteShow'])->name('categories.softDeleteShow');
+
+    // Categories Crud route
+
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
+    Route::delete('/categories/{id}/delete', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::patch('/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
 
 
-// Resource Route
-Route::resources([
-    'products' => ProductController::class,
-    'brands' => BrandController::class,
-    'users' => UserController::class
-]);
+    // products
 
-// Route::resource('products', ProductController::class);
-// Route::resource('brands', BrandController::class);
+    Route::get('/products/pdf', [ProductController::class, 'downloadPdf'])->name('products.pdf');
+    Route::get('/products/excel', [ProductController::class, 'downloadExcel'])->name('products.excel');
 
-Route::fallback(function () {
-    echo "Apnar url thik nai";
+
+    // Softdelte Route
+    Route::get('/products-trash', [ProductController::class, 'trash'])->name('products.trash');
+    Route::get('/products/{id}/restore', [ProductController::class, 'restore'])->name('products.restore');
+    Route::delete('/products/{id}/permanent/delete', [ProductController::class, 'delete'])->name('products.delete');
+    Route::get('/products/{id}/softDeleteShow', [ProductController::class, 'softDeleteShow'])->name('products.softDeleteShow');
+
+
+    // Resource Route
+    Route::resources([
+        'products' => ProductController::class,
+        'brands' => BrandController::class,
+        'users' => UserController::class
+    ]);
+
+    // Route::resource('products', ProductController::class);
+    // Route::resource('brands', BrandController::class);
+
+    // Route::fallback(function () {
+    //     echo "Apnar url thik nai";
+    // });
+
 });
